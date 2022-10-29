8a57e870a51f995bd26ffe3314e076896484e6a9
richardcode <72392252+RKEC@users.noreply.github.com>
2022-10-29 22:03:00 +0100

    Feature[Backend]: Added code to send rep to API

--- shoulder.py
+++ shoulder.py
@@ -1,10 +1,17 @@
-from turtle import color
 import cv2
 import mediapipe as mp
 import numpy as np
+from flask import Flask
+import threading, time, sys
+from flask_restful import Api, Resource
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+app = Flask(__name__)
+api = Api(app)
+
+count = 0
+
+def start_server():
+    app.run()
 
 def calculate_angle(a,b,c):
     a = np.array(a)
@@ -19,67 +26,55 @@ def calculate_angle(a,b,c):
     
     return angle
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)   
 
-count = 0
-direction = None
-message = None
-
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-
-    while cap.isOpened():
-        ret, image = cap.read()
-
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = pose.process(image)
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        try:
-            landmarks = results.pose_landmarks.landmark
-
-            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-            wrist_pos = landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y
-            shoulder_pos = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y
-            
-            angle = calculate_angle(shoulder, elbow, wrist)
-
-            cv2.putText(image, str(angle),
-                            tuple(np.multiply(elbow, [640, 480]).astype(int)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA
-                                )          
+def collect_data():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+    
+    direction = None
+    message = None
  
-            if angle <= 160 and angle >= 150:
-                direction = "up"
-            if angle < 90 and direction == "up":
-                direction="down"
-                count+=1
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5 ) as pose:
+        while cap.isOpened():
+            success, im = cap.read()
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+            im.flags.writeable = False
+            results = pose.process(im)
+            im.flags.writeable = True
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
 
-            if wrist_pos > shoulder_pos - .025:
-                message = "TOO LOW"
-            else:
-                message = "OK"
+            try:
+                global count
+                landmarks = results.pose_landmarks.landmark
 
-        except:
-            pass    
+                shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                # wrist_pos = landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y
+                # shoulder_pos = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y
+                
+                angle = calculate_angle(shoulder, elbow, wrist)   
 
-        cv2.rectangle(image, (0,0), (400,73), (245, 117,16),-1)     
-        cv2.putText(image, str(count), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)   
-        cv2.putText(image, message, (100,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)   
-        
+                if angle <= 160 and angle >= 150:
+                    direction = "up"
+                if angle < 90 and direction == "up":
+                    direction="down"
+                    count+=1
+                    print("captured:", count)
+            except:
+                pass
 
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                        mp_drawing.DrawingSpec(color=(29,162,7), thickness=2, circle_radius=4),
-                        mp_drawing.DrawingSpec(color=(185,191,184), thickness=2, circle_radius=2)
-        )   
+class PublishData(Resource):
+    global count
+    def get(self):
+        print("published:", count)
+        return{"Rep": count}
 
-        cv2.imshow('Raw Webcam Feed', image)
+api.add_resource(PublishData, "/")
+collect = threading.Thread(target=collect_data)
+start = threading.Thread(target=start_server)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":  
+    collect.start()
+    start.start()
