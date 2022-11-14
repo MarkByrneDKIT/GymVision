ca729607b19f090e9a8f7a3bc6fc54c714ae27ab
Liam <d00227509@student.dkit.ie>
2022-11-14 10:30:04 +0000

    Feature[Website]: Front & back end login connected

--- app/src/pages/loginRegister/login.jsx
+++ app/src/pages/loginRegister/login.jsx
@@ -1,6 +1,23 @@
 import "./login.css"
+import { useRef } from "react"
+import {loginCall} from "../../apiCalls"
+import { useContext} from "react"
+import { AuthContext } from "../../context/AuthContext";
+
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
+  
+    const handleClick = (e) => {
+      e.preventDefault();
+      loginCall(
+        { email: email.current.value, password: password.current.value },
+        dispatch
+      );
+      };
+      console.log(user);
     return (
         <div>
             <div className="loginWrapper">
@@ -11,12 +28,9 @@ export default function Login() {
                 </div>
                 <div className="loginRight">
                     <div className="loginBox">
-                    <form className="loginBox">
-                        <input placeholder="Email" type="email" className="loginInput" />
-                        <input
-                            placeholder="Password"
-                            className="loginInput"
-                        />
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Email" type="email" className="loginInput" ref={email} required />
+                        <input placeholder="Password" type="password" className="loginInput" ref={password} required />
                         <button className="loginButton" type='submit'>
                             Log In
                         </button>
