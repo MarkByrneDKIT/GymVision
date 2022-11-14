0a602512ff4b8bbfa921140bfc03c4dc5f6fcddb
Liam <d00227509@student.dkit.ie>
2022-11-14 18:33:25 +0000

    Feature[Website]: Front & back end reg connected

--- app/src/pages/loginRegister/register.jsx
+++ app/src/pages/loginRegister/register.jsx
@@ -1,6 +1,33 @@
 import "./register.css";
+import { useRef } from "react"
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const confirmPassword = useRef();
+  const navigate= useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if(confirmPassword.current.value !== password.current.value){
+      confirmPassword.current.setCustomValidity("Passwords don't match.");
+    } else{
+      const user = {
+      username: username.current.value,
+      email: email.current.value,
+      password: password.current.value
+      };
+      try {
+        await axios.post("/auth/register", user);
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,16 +38,16 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Repeat Password" className="loginInput" />
-            <button className="loginButton">Sign Up</button>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Username" ref={username} className="loginInput" />
+            <input placeholder="Email" ref={email} type="email" className="loginInput" />
+            <input placeholder="Password" ref={password} type="password" className="loginInput" />
+            <input placeholder="Repeat Password" ref={confirmPassword} type="password" className="loginInput" />
+            <button className="loginButton" type="submit">Sign Up</button>
             <button className="loginRegisterButton">
               Log in
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
