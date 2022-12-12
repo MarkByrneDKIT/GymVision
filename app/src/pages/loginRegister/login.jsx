6c5b8136bc4a76ae2a5a0c272f498311d7691bb2
Kealan94 <crilly2k12@hotmail.co.uk>
2022-12-12 21:49:44 +0000

    Updated Login Page

--- app/src/pages/loginRegister/login.jsx
+++ app/src/pages/loginRegister/login.jsx
@@ -6,30 +6,29 @@ import { AuthContext } from "../../context/AuthContext";
 
 
 export default function Login() {
-    const email = useRef();
+    const username = useRef();
     const password = useRef();
     const { user, isFetching, error, dispatch } = useContext(AuthContext);
   
     const handleClick = (e) => {
       e.preventDefault();
       loginCall(
-        { email: email.current.value, password: password.current.value },
+        { username: username.current.value, password: password.current.value },
         dispatch
       );
       };
       console.log(user);
     return (
         <div>
+          <h3 className="loginLogo">SetStats</h3>           
             <div className="loginWrapper">
+            <span className="loginDesc">AI assisted training!</span>
                 <div className="loginLeft">
-                    <h3 className="loginLogo">SetStats</h3>
-                    <span className="loginDesc">AI assisted training!</span>
-
                 </div>
                 <div className="loginRight">
                     <div className="loginBox">
                     <form className="loginBox" onSubmit={handleClick}>
-                        <input placeholder="Email" type="email" className="loginInput" ref={email} required />
+                        <input placeholder="Username" type="username" className="loginInput" ref={username} required />
                         <input placeholder="Password" type="password" className="loginInput" ref={password} required />
                         <button className="loginButton" type='submit'>
                             Log In
