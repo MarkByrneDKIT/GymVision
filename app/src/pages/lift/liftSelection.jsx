841779060a8c1a9b1d3b4e34dcf28d16726b9106
Kealan94 <crilly2k12@hotmail.co.uk>
2022-12-12 22:16:20 +0000

    Feature[UI]:liftselection page updated

--- app/src/pages/lift/liftSelection.jsx
+++ app/src/pages/lift/liftSelection.jsx
@@ -1,14 +1,21 @@
 import "./lift.css"
-
+import { useRef } from "react"
+import {loginCall} from "../../apiCalls"
+import { useContext} from "react"
+import { AuthContext } from "../../context/AuthContext";
+ 
 
 export default function LiftSelection() {
 
     return(
-
+    
         <div>
+                <h1 className="loginLogo">SetStats</h1>    
+                <h2 className="PickLift">Choose your lift</h2>  
             <button>Deadlift</button>
             <button>Squat</button>
             <button>Shoulder Press</button>
+            <button2>View Lift History</button2>
         </div>
 
     );
--- app/src/pages/loginRegister/login.jsx
+++ app/src/pages/loginRegister/login.jsx
@@ -20,16 +20,33 @@ export default function Login() {
       console.log(user);
     return (
         <div>
-          <h3 className="loginLogo">SetStats</h3>           
+             <div class="row">
+  <div class="column">
+    <div class="card">Register</div>
+  </div>
+  <div class="column">
+    <div class="card">Lifts</div>
+  </div>
+  <div class="column">
+    <div class="card">Rep Count</div>
+  </div>
+  <div class="column">
+    <div class="card">Feedback</div>
+  </div>
+</div> 
+          <h3 className="loginLogo">SetStats</h3>      
+           
             <div className="loginWrapper">
-            <span className="loginDesc">AI assisted training!</span>
                 <div className="loginLeft">
                 </div>
+                
                 <div className="loginRight">
                     <div className="loginBox">
                     <form className="loginBox" onSubmit={handleClick}>
-                        <input placeholder="Username" type="username" className="loginInput" ref={username} required />
-                        <input placeholder="Password" type="password" className="loginInput" ref={password} required />
+                        <h4 className= "Username">Username</h4>
+                        <input placeholder="Enter Username" type="username" className="loginInput" ref={username} required />
+                        <h5 className= "Password">Password</h5>
+                        <input placeholder="Enter Password" type="password" className="loginInput" ref={password} required />
                         <button className="loginButton" type='submit'>
                             Log In
                         </button>
