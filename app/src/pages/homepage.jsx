8f955b3d409d1a58ebb36a6d07e177a956d16705
Mark <D00228088@student.dkit.ie>
2023-02-03 21:44:30 +0000

    Feature: Updated Homepage UI

--- app/src/pages/homepage.jsx
+++ app/src/pages/homepage.jsx
@@ -8,21 +8,14 @@ import Navbar from '../components/Navbar/Navbar'
 export default function Homepage() {
     return (
         <div className="container">
-        <Navbar/> 
-        <ul>
-        {/*  <li class="scroll-to-section"><a href="/" class="active">Home</a></li>
-         <li class="scroll-to-section"><a href="/graph/">Graph</a></li>
-         <li class="scroll-to-section"><a href="/sessions/">History</a></li>
-         <li class="scroll-to-section"><a href="/register">Register</a></li>
-         <li class="scroll-to-section"><a href="/logout">Logout</a></li> */}
-        
-        <li><Link to="/deadlift">Deadlift</Link></li>
-        <li><Link to="/squat">Squat</Link></li>
-        <li><Link to="/shoulderpress">Shoulder Press</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        {/* <Link to="/liftselection">Deadlift</Link> */}
-    </ul>
-    </div>
+            <Navbar/> 
+            <button id="buttonOne"><a href='/liftSelection'>Select Lift</a></button>
+            <button id="buttonTwo"><a>Profile</a></button>
+            <button id="buttonThree"><a href='/history'>History</a></button>
+            <button id="buttonFour"><a>Gym Buddies</a></button>
+            <div id="boxOne">Box 1</div>
+            <div id="boxTwo">Box 2</div>
+            <div id="boxThree">Box 3</div>
+        </div>
      );
 }
