3b2cc96d20c0bff630fcb42ea6ea41aa594961be
himanshhh <72358656+himanshhh@users.noreply.github.com>
2023-03-13 19:57:38 +0000

    Fix [Software-Testing]
    cypress test for the updated login page

--- login.cy.js
+++ login.cy.js
@@ -5,10 +5,22 @@ describe('Login', () => {
       cy.visit('http://localhost:3000/login');
   
       //1.Enter username ('liam' in this case)
-      cy.findByRole('textbox').type('liam');
+      cy.findByPlaceholderText(/Username/i).type('liam');
 
       //2. Enter password('password123' in this case)
-      cy.findByPlaceholderText(/enter password/i).type('password123');
+      cy.findByPlaceholderText(/password/i).type('password123');
+
+
+      //cy.findByRole('presentation').check();
+      Cypress.Commands.add("clickRecaptcha", () => {
+        cy.window().then(win => {
+          win.document
+            .querySelector("iframe[src*='recaptcha']")
+            .contentDocument.getElementById("recaptcha-token")
+            .click();
+        });
+      });
+  
 
       //3. Click Login button
       cy.findByRole('button', {  name: /log in/i}).click();
