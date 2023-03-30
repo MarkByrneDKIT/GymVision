fe250d2ffabc1bdeb2c37feb108ebdee0ed0d931
Mark <D00228088@student.dkit.ie>
2023-03-30 17:55:38 +0100

    Feature: Updated History page css of session list

--- app/src/pages/lift/history.jsx
+++ app/src/pages/lift/history.jsx
@@ -1,16 +1,15 @@
+import React from 'react';
 import Navbar from "../../components/Navbar/Navbar";
-import SessionList from '../../components/Sessions/sessionList'
+import SessionList from '../../components/Sessions/sessionList';
+import './history.css';
 
-export default function History()   {
-
-    return (
-        
-        <div className='container'>
-          <Navbar/>
-          <div>
-            <SessionList/>
-          </div>
-        </div>
-
-    );
+export default function History() {
+  return (
+    <div className='container'>
+      <Navbar />
+      <div className="session-list-container">
+        <SessionList />
+      </div>
+    </div>
+  );
 }
