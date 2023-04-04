abf578a7669a1c7191d6810e671eeda3a941cec8
Mark <D00228088@student.dkit.ie>
2023-04-04 14:18:59 +0100

    fix: changed homepage box layout

--- app/src/pages/homepage.jsx
+++ app/src/pages/homepage.jsx
@@ -14,11 +14,15 @@ export default function Homepage() {
         <div className="container">
 
             <div className="content">
-                <div className="contact-form">
-                    <div className="banner">Tutorials</div>
-                    <button className="start-button" onClick={() => navigate('/SquatTut')}>How to Squat</button>
-                    <button className="start-button" onClick={() => navigate('/graphTut')}>How to Read SetStats Graph</button>
-                    <button className="start-button" onClick={() => navigate('/camerasTut')}>How to Setup Cameras</button>
+                <div className="quick-start">
+                    <div className="banner">Quick Start</div>
+                    <button className="start-button">Recovery</button>
+                    <button className="start-button">Light</button>
+                    <button className="start-button">Heavy</button>
+                    <button className="start-button">Free Mode</button>
+                    <button className="start-button">One Rep Max</button>
+                    <button className="start-button">5 X 5</button>
+                    <button className="start-button">Practice</button>
                 </div>
                 <div className="previous-sessions">
                     <div className="banner">Previous Sessions</div>
@@ -33,15 +37,12 @@ export default function Homepage() {
                     <div className="banner">Latest Session Details</div>
                     <PreviousSessionDetails/>
                 </div>
-                <div className="quick-start">
-                    <div className="banner">Quick Start</div>
-                    <button className="start-button">Recovery</button>
-                    <button className="start-button">Light</button>
-                    <button className="start-button">Heavy</button>
-                    <button className="start-button">Free Mode</button>
-                    <button className="start-button">One Rep Max</button>
-                    <button className="start-button">5 X 5</button>
-                    <button className="start-button">Practice</button>
+                
+                <div className="contact-form">
+                    <div className="banner">Tutorials</div>
+                    <button className="start-button" onClick={() => navigate('/SquatTut')}>How to Squat</button>
+                    <button className="start-button" onClick={() => navigate('/graphTut')}>How to Read SetStats Graph</button>
+                    <button className="start-button" onClick={() => navigate('/camerasTut')}>How to Setup Cameras</button>
                 </div>
             </div>
 
