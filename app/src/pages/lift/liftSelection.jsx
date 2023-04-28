42973614bd221710ac5bd7869d8b167ea0f57043
RKEC <d00230925@student.dkit.ie>
2023-04-28 17:06:12 +0100

    Fix[Website]: Fixed lift select not sending data

--- app/src/pages/lift/liftSelection.jsx
+++ app/src/pages/lift/liftSelection.jsx
@@ -1,9 +1,30 @@
 import "./lift.css";
 import Navbar from "../../components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
+import PubNub from 'pubnub';
 
 export default function LiftSelection() {
-  const navigate = useNavigate();
+      const navigate = useNavigate();
+
+    const pubnub = new PubNub({
+        subscribeKey: 'sub-c-66e361b6-c13c-411e-a780-9b16fc2e0c36',
+        publishKey: 'pub-c-d8d5b759-3b66-4d5b-ae7d-b119cc474e80',
+        userId: 'liamdenningsetstats',
+      });
+
+      const handleClick = async (e, rep, set) => {
+          pubnub.publish(
+            {
+              channel: 'Setstats',
+              message: { reps: rep, sets: set},
+            },
+            function (status, response) {
+              console.log(status);
+              console.log(response);
+            },
+          );
+          navigate('/squat');
+      };
 
   return (
     <div>
@@ -11,13 +32,12 @@ export default function LiftSelection() {
     <div className="container">
       <div className="content">
         <div className="lift-selection">
-          <button className="start-button" onClick={() => navigate("/squat")}>Squat</button>
-          <button className="start-button" onClick={() => navigate("/deadlift")}>Recovery</button>
-          <button className="start-button" onClick={() => navigate("/shoulderPress")}>Light</button>
-          <button className="start-button" onClick={() => navigate("/squat")}>Heavy</button>
-          <button className="start-button" onClick={() => navigate("/deadlift")}>5 X 5</button>
-          <button className="start-button" onClick={() => navigate("/deadlift")}>One Rep Max</button>
-          <button className="start-button" onClick={() => navigate("/shoulderPress")}>Practice</button>
+        <div className="banner">Quick Start</div>
+            <button className="start-button" onClick={event => handleClick(event,3,3)}>Heavy</button>
+            <button className="start-button" onClick={event => handleClick(event, 12,3)}>Light</button>
+            <button className="start-button" onClick={event => handleClick(event, 6,3)}>Muscle Gain</button>
+            <button className="start-button" onClick={event => handleClick(event, 5,5)} >5 x 5</button>
+            <button className="start-button" onClick={event => handleClick(event, 1000, 2)}>Practice</button>
         </div>
       </div>
     </div>
